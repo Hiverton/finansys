18a969abca48b904f15e98ad270e4e1562f1294f
Hiverton Alves <hiverton.alves@gmail.com>
2019-01-25 13:39:28 -0200

    Category service

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -9,7 +9,7 @@ export class InMemoryDataBase implements InMemoryDbService {
             {id: 2, name: "Saúde", description: "Plano de saúde e remédios"},
             {id: 3, name: "Lazer", description: "Cinema, parques, praia, etc"},
             {id: 4, name: "Salário", description: "Recebimento de salário"},
-            {id: 5, name: "Freelas", description: "Trabalho como freelancer"},
+            {id: 5, name: "Freelas", description: "Trabalho como freelancer"}
         ];
 
         return { categories };
