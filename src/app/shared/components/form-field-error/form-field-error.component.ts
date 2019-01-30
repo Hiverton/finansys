a02dc1f2031c09e1164f4f8aaba7c933b81eb28f
Hiverton Alves <hiverton.alves@gmail.com>
2019-01-30 13:58:30 -0200

    component error

---
+++ src/app/shared/components/form-field-error/form-field-error.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { identifierModuleUrl } from '@angular/compiler';
+
+@Component({
+  selector: 'app-form-field-error',
+  template: `
+    <p class="text-danger">
+      {{errorMessage}}
+    </p>
+  `,
+  styleUrls: ['./form-field-error.component.css']
+})
+export class FormFieldErrorComponent implements OnInit {
+
+  @Input('form-control') formControl: FormControl;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public get errorMessage(): string | null {
+    if(this.mustShowMessageError()){
+      return this.getErrorMessage();
+    } else {
+      return null;
+    }
+  }
+
+  private mustShowMessageError(): boolean {
+    return this.formControl.invalid && this.formControl.touched
+  }
+
+  private getErrorMessage(): string | null {
+
+    if(this.formControl.errors.required)
+      return "dado obrigatório"
+    
+    else if(this.formControl.errors.email)
+      return "formato de e-mail inválido"
+
+    else if(this.formControl.errors.minlength) {
+      const requiredLength = this.formControl.errors.minlength.requiredLength
+      return `deve ter no minimo ${requiredLength} caracteres`;
+
+    }else if(this.formControl.errors.maxlength) {
+      const requiredLength = this.formControl.errors.maxlength.requiredLength
+      return `deve ter no máximo ${requiredLength} caracteres`;
+    }
+
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { IMaskModule } from 'angular-imask';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
 
 
 @NgModule({
-  declarations: [BreadCrumbComponent, PageHeaderComponent],
+  declarations: [BreadCrumbComponent, PageHeaderComponent, FormFieldErrorComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -24,7 +25,8 @@ import { PageHeaderComponent } from './components/page-header/page-header.compon
     IMaskModule,
     RouterModule,
     BreadCrumbComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    FormFieldErrorComponent
   ]
 })
 export class SharedModule { }
